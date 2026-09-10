f719cef84efafe8a4fe5a90cfbc2dbd1d773cea4
baizhihe <3165316308@qq.com>
2026-09-10 23:20:38 +0800

    Create Luck.cpp to evaluate luck based on user input
    Add a program to calculate today's luck value based on user input.

---
+++ Luck.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	int a,b; 
+	cout<<"来算算今天的幸运值吧！"<<endl;
+	cout<<"今天多少次感觉自己幸运？"<<endl; 
+	cin>>a;
+	cout<<"今天多少次感觉自己倒霉？"<<endl;
+	cin>>b;
+	if(a==0&&b==0) {cout<<"无事发生？平平淡淡才是真~"<<endl;return 0; 
+	}
+	else{
+	int luck= (100+a*3-b*5);
+	cout<<"今天的幸运值为:" <<luck<<endl; 
+	if(luck>=80&&luck<=100) {cout<<"今天的命运掌握在你自己手里"<<endl;
+	};
+	if(luck<80) {cout<<"你觉得今天不宜出门"<<endl;
+	}
+	if(luck>100){cout<<"你觉得今天该去买彩票"<<endl;
+	}}
+	return 0;
+}
